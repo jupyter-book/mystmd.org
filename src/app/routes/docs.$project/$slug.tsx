9060f8fdc79a92b0cfed884173dd6adc171a2194
Rowan Cockett <rowanc1@gmail.com>
2022-11-16 09:59:11 -0700

    ⏫ Upgrade theme-base

--- src/app/routes/docs.$project/$slug.tsx
+++ src/app/routes/docs.$project/$slug.tsx
@@ -9,32 +9,13 @@ import {
   useNavigationHeight,
   DocumentOutline,
   ContentBlocks,
-  DoiText,
-  Journal,
-  GitHubLink,
   Bibliography,
-  OpenAccessBadge,
-  AuthorsList,
-  ExternalOrInternalLink,
 } from '@curvenote/site';
 import { getArticlePage } from '~/utils/loaders.server';
-import { ArrowTopRightOnSquareIcon as ExternalLinkIcon } from '@heroicons/react/24/outline';
-import { DocumentArrowDownIcon } from '@heroicons/react/24/outline';
-import {
-  Link,
-  NavLink,
-  useLoaderData,
-  useLocation,
-  useParams,
-  useTransition,
-} from '@remix-run/react';
-import type { ManifestProjectPage, SiteManifest } from '@curvenote/site-common';
-import { ReferencesProvider, SiteProvider, useSiteManifest } from '@curvenote/ui-providers';
+import { useLoaderData, useTransition } from '@remix-run/react';
+import type { SiteManifest } from '@curvenote/site-common';
+import { ReferencesProvider } from '@curvenote/ui-providers';
 import { ArticlePageCatchBoundary } from '@curvenote/site';
-import { extractPart, copyNode } from 'myst-common';
-import classNames from 'classnames';
-import { LicenseBadges } from '@curvenote/icons';
-import type { PageFrontmatter } from 'myst-frontmatter';
 import { ArticleWithProviders } from '../../components/Page';
 import { useEffect, useRef, useState } from 'react';
 
@@ -77,38 +58,13 @@ export function useSticky<T extends HTMLElement = HTMLDivElement>() {
   return { ref, sticky };
 }
 
-function FloatingOutline({ height }: { height?: number }) {
-  const { ref, sticky } = useSticky();
+function ArticlePage({ article }: { article: PageLoader }) {
   return (
     <>
-      <div ref={ref} className="h-0 p-0" />
-      <div
-        className={classNames('column-page xl:pl-[100px] hidden xl:block', { relative: !sticky })}
-      >
-        <DocumentOutline
-          top={35}
-          height={height}
-          className={classNames('max-w-[200px]', {
-            'fixed top-0': sticky,
-            absolute: !sticky,
-          })}
-        />
-      </div>
-    </>
-  );
-}
-
-export function ArticlePage({ article }: { article: PageLoader }) {
-  return (
-    <ReferencesProvider
-      references={{ ...article.references, article: article.mdast }}
-      frontmatter={article.frontmatter}
-      urlbase="/docs"
-    >
       <ContentBlocks mdast={article.mdast} />
       <Bibliography />
       <FooterLinksBlock links={article.footer} />
-    </ReferencesProvider>
+    </>
   );
 }
 
@@ -116,9 +72,12 @@ export default function Page() {
   const { ref, height } = useNavigationHeight();
   const article = useLoaderData<PageLoader>() as PageLoader;
   return (
-    <>
+    <ReferencesProvider
+      references={{ ...article.references, article: article.mdast }}
+      frontmatter={article.frontmatter}
+      urlbase="/docs"
+    >
       <TableOfContents
-        urlbase="/docs"
         top={DEFAULT_NAV_HEIGHT}
         height={height + DEFAULT_NAV_HEIGHT - 12}
         showFooter={false}
@@ -131,7 +90,7 @@ export default function Page() {
         </ArticleWithProviders>
         <DocumentOutline top={DEFAULT_NAV_HEIGHT + 50} height={height + DEFAULT_NAV_HEIGHT - 12} />
       </main>
-    </>
+    </ReferencesProvider>
   );
 }
 

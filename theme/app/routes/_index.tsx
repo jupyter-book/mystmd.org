222af3c4f3c31ad95686d67350520a07f3ea1343
Rowan Cockett <rowanc1@gmail.com>
2024-03-22 07:43:14 -0600

    🚀 Upgrade to v0.8.0 (#14)
    Co-authored-by: stevejpurves <steve@curvenote.com>

--- theme/app/routes/_index.tsx
+++ theme/app/routes/_index.tsx
@@ -1,11 +1,13 @@
 import type { LinksFunction, LoaderFunction, MetaFunction } from '@remix-run/node';
 import type { PageLoader } from '@myst-theme/common';
 import { getMetaTagsForArticle, KatexCSS, ArticlePage } from '@myst-theme/site';
+import { ComputeOptionsProvider, ThebeLoaderAndServer } from '@myst-theme/jupyter';
 import { getPage } from '~/utils/loaders.server';
 import { useLoaderData } from '@remix-run/react';
 import type { SiteManifest } from 'myst-config';
 import { ArticleAndNavigation, HeaderSection, NavigationAndFooter } from '../components/Page';
 import { Error404 } from '../components/Error404';
+import { ProjectProvider } from '@myst-theme/providers';
 
 export const meta: MetaFunction = (args) => {
   const config = args.parentsData?.root?.config as SiteManifest | undefined;
@@ -31,9 +33,17 @@ export default function LandingPage() {
   (article.frontmatter as any).options = { hide_title_block: true, hide_footer_links: true };
   return (
     <ArticleAndNavigation header={<HeaderSection />} hide_toc>
-      <main className="article content article-grid grid-gap">
-        <ArticlePage article={article} />
-      </main>
+      <ProjectProvider>
+        <ComputeOptionsProvider
+          features={{ notebookCompute: true, figureCompute: true, launchBinder: false }}
+        >
+          <ThebeLoaderAndServer baseurl={''}>
+            <main className="article content article-grid grid-gap">
+              <ArticlePage article={article} />
+            </main>
+          </ThebeLoaderAndServer>
+        </ComputeOptionsProvider>
+      </ProjectProvider>
     </ArticleAndNavigation>
   );
 }
--- theme/app/routes/overview.($slug).tsx
+++ theme/app/routes/overview.($slug).tsx
@@ -1,10 +1,11 @@
 import type { LoaderFunction } from '@remix-run/node';
 import type { PageLoader } from '@myst-theme/common';
 import { ArticlePage } from '@myst-theme/site';
+import { ComputeOptionsProvider, ThebeLoaderAndServer } from '@myst-theme/jupyter';
 import { getPage } from '../utils/loaders.server';
 import { NavLink, useLoaderData } from '@remix-run/react';
 import { ArticleAndNavigation, NavigationAndFooter } from '../components/Page';
-import { BaseUrlProvider, useSiteManifest } from '@myst-theme/providers';
+import { BaseUrlProvider, ProjectProvider, useSiteManifest } from '@myst-theme/providers';
 import classNames from 'classnames';
 import { Error404 } from '../components/Error404';
 
@@ -23,32 +24,40 @@ export default function ContentPage() {
   return (
     <ArticleAndNavigation hide_toc>
       <BaseUrlProvider baseurl={`/${baseurl}`}>
-        <main className="article article-grid grid-gap py-[100px]">
-          <h1 className="text-center">{article.frontmatter.title}</h1>
-          <div className="col-body-outset">
-            <div className="border-y border-gray-200 py-3 my-[100px] flex flex-row justify-around">
-              {site?.projects?.[0].pages
-                .filter((p) => 'slug' in p)
-                .map((p) => {
-                  if (p.level === 1)
-                    return (
-                      <NavLink
-                        key={p.slug}
-                        to={`/${baseurl}/${p.slug}`}
-                        prefetch="intent"
-                        className={({ isActive }) =>
-                          classNames('no-underline', { 'text-blue-600': isActive })
-                        }
-                      >
-                        {p.title}
-                      </NavLink>
-                    );
-                  return null;
-                })}
-            </div>
-          </div>
-          <ArticlePage article={article} />
-        </main>
+        <ProjectProvider>
+          <ComputeOptionsProvider
+            features={{ notebookCompute: true, figureCompute: true, launchBinder: false }}
+          >
+            <ThebeLoaderAndServer baseurl={''}>
+              <main className="article article-grid grid-gap py-[100px]">
+                <h1 className="text-center">{article.frontmatter.title}</h1>
+                <div className="col-body-outset">
+                  <div className="border-y border-gray-200 py-3 my-[100px] flex flex-row justify-around">
+                    {site?.projects?.[0].pages
+                      .filter((p) => 'slug' in p)
+                      .map((p) => {
+                        if (p.level === 1)
+                          return (
+                            <NavLink
+                              key={p.slug}
+                              to={`/${baseurl}/${p.slug}`}
+                              prefetch="intent"
+                              className={({ isActive }) =>
+                                classNames('no-underline', { 'text-blue-600': isActive })
+                              }
+                            >
+                              {p.title}
+                            </NavLink>
+                          );
+                        return null;
+                      })}
+                  </div>
+                </div>
+                <ArticlePage article={article} />
+              </main>
+            </ThebeLoaderAndServer>
+          </ComputeOptionsProvider>
+        </ProjectProvider>
       </BaseUrlProvider>
     </ArticleAndNavigation>
   );

fd0afe7a34391a7dad85c5cd77d8ce20b1a02bec
Rowan Cockett <rowanc1@gmail.com>
2025-03-05 20:42:40 -0700

    Respect template options

--- theme/app/routes/$project.($slug).tsx
+++ theme/app/routes/$project.($slug).tsx
@@ -12,9 +12,16 @@ import { MyST } from 'myst-to-react';
 import { FrontmatterBlock } from '@myst-theme/frontmatter';
 import { ComputeOptionsProvider, ThebeLoaderAndServer } from '@myst-theme/jupyter';
 import { useLoaderData } from '@remix-run/react';
-import { ProjectProvider, ArticleProvider, useThemeTop } from '@myst-theme/providers';
+import {
+  ProjectProvider,
+  ArticleProvider,
+  useThemeTop,
+  useSiteManifest,
+} from '@myst-theme/providers';
 import { getPage } from '~/utils/loaders.server';
 import { ArticleWithProviders } from '../components/Page';
+import type { TemplateOptions } from '../types';
+import type { SiteManifest } from 'myst-config';
 
 export const meta: V2_MetaFunction = ({ data, location }) => {
   if (!data) return [];
@@ -37,22 +44,18 @@ export const loader: LoaderFunction = async ({ params }) => {
   return page;
 };
 
-function ArticlePage({ article }: { article: PageLoader }) {
-  return (
-    <>
-      <div id="skip-to-article" />
-      <MyST ast={article.mdast} />
-      <Footnotes />
-      <Bibliography />
-      <FooterLinksBlock links={article.footer} />
-    </>
-  );
-}
-
 export default function Page() {
   const { container, outline } = useOutlineHeight();
   const article = useLoaderData<PageLoader>() as PageLoader;
   const top = useThemeTop();
+
+  const pageDesign: TemplateOptions = (article.frontmatter as any)?.site ?? {};
+  const siteDesign: TemplateOptions =
+    (useSiteManifest() as SiteManifest & TemplateOptions)?.options ?? {};
+  const { hide_title_block, hide_footer_links, hide_outline, outline_maxdepth } = {
+    ...siteDesign,
+    ...pageDesign,
+  };
 
   return (
     <ArticleProvider
@@ -67,23 +70,32 @@ export default function Page() {
           <ThebeLoaderAndServer baseurl={''}>
             <main ref={container}>
               <ArticleWithProviders article={article}>
-                <FrontmatterBlock
-                  kind={article.kind}
-                  frontmatter={article.frontmatter}
-                  className="pt-9"
-                />
-                <div
-                  className="sticky z-10 hidden h-0 pt-5 ml-10 col-margin-right lg:block"
-                  style={{ top }}
-                >
-                  <DocumentOutline
-                    top={16}
-                    className="relative lg:block"
-                    outlineRef={outline}
-                    isMargin={false}
+                {!hide_title_block && (
+                  <FrontmatterBlock
+                    kind={article.kind}
+                    frontmatter={article.frontmatter}
+                    className="pt-9"
                   />
-                </div>
-                <ArticlePage article={article} />
+                )}
+                {!hide_outline && (
+                  <div
+                    className="sticky z-10 hidden h-0 pt-5 ml-10 col-margin-right lg:block"
+                    style={{ top }}
+                  >
+                    <DocumentOutline
+                      top={16}
+                      className="relative lg:block"
+                      outlineRef={outline}
+                      maxdepth={outline_maxdepth}
+                      isMargin={false}
+                    />
+                  </div>
+                )}
+                <div id="skip-to-article" />
+                <MyST ast={article.mdast} />
+                <Footnotes />
+                <Bibliography />
+                {!hide_footer_links && <FooterLinksBlock links={article.footer} />}
               </ArticleWithProviders>
             </main>
           </ThebeLoaderAndServer>
